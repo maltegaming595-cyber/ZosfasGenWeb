9ee91eaddf2f8019b28540d5e774b9669bcc0cf0
maltegaming595-cyber <maltegaming595@gmail.com>
2026-02-01 04:29:13 +0100

    Delete src directory

--- src/models/TicketCounter.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const TicketCounterSchema = new mongoose.Schema(
-  { key: { type: String, unique: true }, value: { type: Number, default: 0 } },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("TicketCounter", TicketCounterSchema);
